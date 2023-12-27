bc48a062497eac4f8edc8fed76ec64ff3f12fa02
= <itzmohamed007@gmail.com>
2023-12-28 00:03:43 +0100

    added agent service interface

---
+++ MyRHApi/src/main/java/com/myrh/dtos/requests/ReqAgent.java
@@ -0,0 +1,11 @@
+package com.myrh.dtos.requests;
+
+import com.myrh.dtos.noRelations.EmptyUser;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ReqAgent extends EmptyUser {
+
+}
---
+++ MyRHApi/src/main/java/com/myrh/services/interfaces/IAgentService.java
@@ -0,0 +1,8 @@
+package com.myrh.services.interfaces;
+
+import com.myrh.dtos.requests.ReqAgent;
+import com.myrh.dtos.responses.ResAgent;
+
+public interface IAgentService extends IGlobalService<ReqAgent, ResAgent, String> {
+
+}
