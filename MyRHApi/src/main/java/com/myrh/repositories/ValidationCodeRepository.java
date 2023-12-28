d6a889797ea7ca37944735a42428d15453cc03f5
= <itzmohamed007@gmail.com>
2023-12-28 09:59:10 +0100

    added finding validation code by code method in repository

--- MyRHApi/src/main/java/com/myrh/repositories/ValidationCodeRepository.java
+++ MyRHApi/src/main/java/com/myrh/repositories/ValidationCodeRepository.java
@@ -4,9 +4,10 @@ import com.myrh.models.ValidationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ValidationCodeRepository extends JpaRepository<ValidationCode, UUID> {
-
+    Optional<ValidationCode> findValidationCodeByCode(String code);
 }
--- MyRHApi/src/main/java/com/myrh/services/ValidationCodeService.java
+++ MyRHApi/src/main/java/com/myrh/services/ValidationCodeService.java
@@ -1,7 +1,32 @@
 package com.myrh.services;
 
+import com.myrh.exceptions.ResourceNotFoundException;
+import com.myrh.models.ValidationCode;
+import com.myrh.repositories.ValidationCodeRepository;
 import com.myrh.services.interfaces.IValidationCodeService;
+import com.myrh.utils.Utils;
+import jdk.jshell.execution.Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ValidationCodeService implements IValidationCodeService {
+    private final ValidationCodeRepository repository;
 
+    @Override
+    public String read(String stringUuid) {
+        ValidationCode validationCode = repository.findById(Utils.parseStringToUuid(stringUuid))
+                .orElseThrow(() -> new ResourceNotFoundException("Validation code not found"));
+    }
+
+    @Override
+    public String create(ValidationCode validationCode) {
+        return null;
+    }
+
+    @Override
+    public void delete(String stringUuid) {
+
+    }
 }
