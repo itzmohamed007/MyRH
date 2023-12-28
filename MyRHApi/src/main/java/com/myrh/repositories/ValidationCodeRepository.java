644727cedd4ce03841b83fb6b200b5018f9d3c3c
= <itzmohamed007@gmail.com>
2023-12-28 09:54:45 +0100

    added validation code repository

---
+++ MyRHApi/src/main/java/com/myrh/repositories/ValidationCodeRepository.java
@@ -0,0 +1,12 @@
+package com.myrh.repositories;
+
+import com.myrh.models.ValidationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ValidationCodeRepository extends JpaRepository<ValidationCode, UUID> {
+
+}
